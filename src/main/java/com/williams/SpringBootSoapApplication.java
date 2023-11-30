61ba2ab0eeb0ee2deb34b8e8121acdba2bae65dc
willbipo <willbipo@gmail.com>
2023-11-30 16:50:57 -0600

    test soap servie with commandLineRunner

--- src/main/java/com/williams/SpringBootSoapApplication.java
+++ src/main/java/com/williams/SpringBootSoapApplication.java
@@ -1,13 +1,31 @@
 package com.williams;
 
+import com.williams.client.SoapClient;
+import com.williams.wsdl.AddResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootSoapApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSoapApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(SoapClient soapClient){
+		return args -> {
+			AddResponse response = soapClient.addMethod(4,67);
+			log.info("El resultado de {} y {} es: {}", 4,67,response.getAddResult());
+
+		};
 	}
 
 }
