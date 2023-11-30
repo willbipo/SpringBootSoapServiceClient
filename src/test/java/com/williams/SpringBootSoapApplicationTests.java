50c157ac94d59757a670846eb95227e3d41396fe
willbipo <willbipo@gmail.com>
2023-11-30 10:23:08 -0600

    first commit

---
+++ src/test/java/com/williams/SpringBootSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
